4fb4a11d56278104ad40ac52cff3e61f89f74851
adm1228 <116378510+adm1228@users.noreply.github.com>
2022-10-22 00:18:01 +0700

    Update setting.js

--- setting.js
+++ setting.js
@@ -7,21 +7,21 @@ global.Api = {
 }
 
 // Apikey Website Api
-global.ApiKey = 'coderevolution' //JIKA INGIN MENDAPATKAN KOMISI DAFTAR DI HTTPS://AMPIBISMM.MY.ID UNTUK MENDAPATKAN APIKEY!
+global.ApiKey = 'iNnZEBh6ZsglQqLypYDAdPdy8bNEo9' //JIKA INGIN MENDAPATKAN KOMISI DAFTAR DI HTTPS://AMPIBISMM.MY.ID UNTUK MENDAPATKAN APIKEY!
 
 global.smm = {
-  nomor : '6283102650464',
-  atasnama : 'Ahmad Nur Fadhil'
+  nomor : '6281214557752',
+  atasnama : 'Adam'
 }
 
 // Other
-global.owner = ['6283102650464',"6282135724604"]
-global.premium = ['6283102650464']
-global.profilebio = ['6283102650464'] // ini ganti nomor lu ntar bisa nampilin pp lu
+global.owner = ['6281214557752',"6281214557752"]
+global.premium = ['6281214557752']
+global.profilebio = ['6281214557752'] // ini ganti nomor lu ntar bisa nampilin pp lu
 global.sponsor = 'no sponsors in:('
-global.packname = 'Crizzy-Bot'
-global.footer = '© 2022 Fadhil Graphy'
-global.author = 'Owner : Fadhil Graphy\nIg1 : @fdhlgrphy\nIg2 : @nr.fdhl__'
+global.packname = 'Kenzo-Botz'
+global.footer = '© 2022 Adam'
+global.author = 'Owner : Adam'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.act = {
 		wait: 'please wait a moment, processing data...',
